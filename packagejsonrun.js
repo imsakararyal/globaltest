6b1245c0f993b0b2573c5189bdd350c44c5155dd
Sakar Aryal <imsakararyal@gmail.com>
2020-09-29 09:41:17 +0545

    ddsds

--- packagejsonrun.js
+++
@@ -1,4 +0,0 @@
-   /* "android-rumahaman": "ENV=development ./envscript.sh && ENVFILE=.env.dev react-native run-android",
-    "android-tibaybalay": "ENV=production ./envscript.sh && ENVFILE=.env.prod react-native run-android",
-    "build-android-prod": "ENVFILE=.env.prod ./envscript.sh && cd android && ./gradlew assembleRelease && cd .."
-*/
